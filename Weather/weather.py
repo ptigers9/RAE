b22ee7ebda571cbeddde7dda5a46d1a1d8e2adcb
ptigers9 <doug-moore@utulsa.edu>
2016-10-04 17:57:39 -0500

    Fixed the problem with merging day weather dictionaries and added all cities to the Locations.csv

--- Weather/weather.py
+++ Weather/weather.py
@@ -6,7 +6,7 @@ import datetime
 import sys
 import itertools
 
-def load_day_dict(url):
+def get_day_dict(url):
 	csv_response = urllib2.urlopen(url)
 	csv_obj = csv.reader(csv_response)
 	rows = []
@@ -32,9 +32,12 @@ def load_day_dict(url):
 	
 	return day_dict
 
-def get_city_daily_weather(city = 'Tulsa', state = 'Oklahoma', zip = 74104):
-	log = open('Logs\load_weather_log_{dt}.txt'.format(dt=str(datetime.date.today())), 'a')
-	log.write('--------------------------------------------------------\n')
+def get_city_daily_weather(city = 'Tulsa', state = 'Oklahoma', zip = 74104,
+	start_yr = 2016, start_month = 10, start_day = 1):
+	starttime = datetime.datetime.today()
+	
+	log = open('Logs\load_weather_log_{dt}_{city}_{state}_{zip}.txt'.format(dt=str(datetime.date.today()),city=city,state=state,zip=zip), 'a')
+	log.write('\n--------------------------------------------------------\n')
 	log.write('Running on {dt}\n'.format(dt=str(datetime.datetime.today())))
 
 	day_data_dicts = []
@@ -43,9 +46,8 @@ def get_city_daily_weather(city = 'Tulsa', state = 'Oklahoma', zip = 74104):
 
 	url = selected_city['Url'][0]
 	
-	#Loop through the days from 1970 to today() and call load_day_dict
-	#historical_dt = datetime.date(1970,1,1)
-	historical_dt = datetime.date(1970,1,1)
+	#Loop through the days from 1970 to today() and call get_day_dict
+	historical_dt = datetime.date(start_yr, start_month, start_day)
 	today_dt = datetime.date.today()
 
 	while historical_dt < today_dt:
@@ -58,7 +60,7 @@ def get_city_daily_weather(city = 'Tulsa', state = 'Oklahoma', zip = 74104):
 		print "Loading data for {dt} using {url}\n".format(url=full_url,dt=historical_dt)
 		
 		try:
-			day_dict = load_day_dict(full_url)
+			day_dict = get_day_dict(full_url)
 			day_data_dicts.append(day_dict)
 			
 			log.write("Successfully loaded\n")
@@ -67,25 +69,25 @@ def get_city_daily_weather(city = 'Tulsa', state = 'Oklahoma', zip = 74104):
 		
 		historical_dt = historical_dt + datetime.timedelta(1)
 	
-	all_dict_data = { k:[d[k] for d in day_data_dicts] for k in day_data_dicts[0] }
+	days_df = pd.DataFrame()
 	
-	final_dict = {}
+	for day_dict in day_data_dicts:
+		days_df = days_df.append(pd.DataFrame(day_dict))
 	
-	for key, value in all_dict_data.iteritems():
-		#Merge all arrays here and assign to new dictionary
-		final_dict[key] = list(itertools.chain.from_iterable(value))
+	file_name = 'Data\historical_weather_{city}_{state}_{zip}.csv'.format(city=city,state=state,zip=zip).replace(' ','')
 	
-	days_df = pd.DataFrame(final_dict)
-	file_name = 'historical_weather_{city}_{state}_{zip}.csv'.format(city=city,state=state,zip=zip)
-	print file_name
+	
 	days_df.to_csv(file_name)
 	
+	endtime = datetime.datetime.today()
+	elapsed_time = endtime - starttime	
+	
 	log.write("\nCreated csv: {0}\n".format(file_name))
+	log.write("\nTime to complete data load: {0}\n".format(str(elapsed_time)))
 	log.write('--------------------------------------------------------\n')
 	log.close()
-		
+	
 	return days_df
-
 
 
 
