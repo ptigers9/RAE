82541c0ed14d88bf4e9b66687a5899162c2bb9b4
ptigers9 <doug-moore@utulsa.edu>
2016-10-05 00:20:07 -0500

    Loaded 2016 data for all cities

--- Weather/weather.py
+++ Weather/weather.py
@@ -33,8 +33,13 @@ def get_day_dict(url):
 	return day_dict
 
 def get_city_daily_weather(city = 'Tulsa', state = 'Oklahoma', zip = 74104,
-	start_yr = 1970, start_month = 1, start_day = 1):
+	start_yr = 1970, start_month = 1, start_day = 1,
+	end_yr = datetime.date.today().year, end_month = datetime.date.today().month, end_day = datetime.date.today().day):
 	starttime = datetime.datetime.today()
+	
+	#Loop through the days from 1970 to today() and call get_day_dict
+	start_date = datetime.date(start_yr, start_month, start_day)
+	end_date = datetime.date(end_yr, end_month, end_day)
 	
 	log = open('Logs\load_weather_log_{dt}_{city}_{state}_{zip}.txt'.format(dt=str(datetime.date.today()),city=city,state=state,zip=zip), 'a')
 	log.write('\n--------------------------------------------------------\n')
@@ -52,18 +57,14 @@ def get_city_daily_weather(city = 'Tulsa', state = 'Oklahoma', zip = 74104,
 
 	url = selected_city['Url'].item()
 	
-	#Loop through the days from 1970 to today() and call get_day_dict
-	historical_dt = datetime.date(start_yr, start_month, start_day)
-	today_dt = datetime.date.today()
-
-	while historical_dt < today_dt:
-		day = historical_dt.day
-		month = historical_dt.month
-		year = historical_dt.year
+	while start_date < end_date:
+		day = start_date.day
+		month = start_date.month
+		year = start_date.year
 
 		full_url = url.format(year=year, month=month, day=day)
-		log.write("\nLoading data for {dt} using {url}\n".format(url=full_url,dt=historical_dt))
-		print "Loading data for {dt} using {url}\n".format(url=full_url,dt=historical_dt)
+		log.write("\nLoading data for {dt} using {url}\n".format(url=full_url,dt=start_date))
+		print "Loading data for {dt} using {url}\n".format(url=full_url,dt=start_date)
 		
 		try:
 			day_dict = get_day_dict(full_url)
@@ -73,7 +74,7 @@ def get_city_daily_weather(city = 'Tulsa', state = 'Oklahoma', zip = 74104,
 		except:
 			log.write("ERROR:{0}\n".format(sys.exc_info()[0]))
 		
-		historical_dt = historical_dt + datetime.timedelta(1)
+		start_date = start_date + datetime.timedelta(1)
 	
 	days_df = pd.DataFrame()
 	
@@ -81,7 +82,9 @@ def get_city_daily_weather(city = 'Tulsa', state = 'Oklahoma', zip = 74104,
 		days_df = days_df.append(pd.DataFrame(day_dict))
 	
 	file_name = 'Data\historical_weather_{city}_{state}_{zip}.csv'.format(city=city,state=state,zip=zip).replace(' ','')
-	
+	days_df['City'] = city
+	days_df['State'] = state
+	days_df['Zip'] = zip
 	
 	days_df.to_csv(file_name)
 	
