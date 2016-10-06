75f6fe6924a4450b0ad3eeec14fa2afcc4fee6d1
ptigers9 <doug-moore@utulsa.edu>
2016-10-05 21:26:27 -0500

    Added the ability to find missing dates for cities and only run and add those to the csv

--- Weather/weather.py
+++ Weather/weather.py
@@ -32,6 +32,33 @@ def get_day_dict(url):
 	
 	return day_dict
 
+def get_day_dicts_df(urls, log, city, state, zip):
+
+	day_data_dicts = []
+	
+	for url in urls:
+		log.write("Loading URL: {url}\n".format(url=url))
+		
+		try:
+			day_dict = get_day_dict(url)
+			day_data_dicts.append(day_dict)
+			
+			log.write("Successfully loaded\n\n")
+		except:
+			log.write("ERROR:{0}\n\n".format(sys.exc_info()[0]))
+			
+	days_df = pd.DataFrame()
+	
+	for day_dict in day_data_dicts:
+		day_dict_df = pd.DataFrame(day_dict)
+		days_df = days_df.append(day_dict_df)
+	
+	days_df['City'] = city
+	days_df['State'] = state
+	days_df['Zip'] = zip
+	
+	return days_df
+	
 def get_city_daily_weather(city = 'Tulsa', state = 'Oklahoma', zip = 74104,
 	start_yr = 1970, start_month = 1, start_day = 1,
 	end_yr = datetime.date.today().year, end_month = datetime.date.today().month, end_day = datetime.date.today().day):
@@ -41,21 +68,13 @@ def get_city_daily_weather(city = 'Tulsa', state = 'Oklahoma', zip = 74104,
 	start_date = datetime.date(start_yr, start_month, start_day)
 	end_date = datetime.date(end_yr, end_month, end_day)
 	
-	log = open('Logs\load_weather_log_{dt}_{city}_{state}_{zip}.txt'.format(dt=str(datetime.date.today()),city=city,state=state,zip=zip), 'a')
+	#log = open('Logs/load_weather_log_{dt}_{city}_{state}_{zip}.txt'.format(dt=str(datetime.date.today()),city=city,state=state,zip=zip), 'a')
+	log = open('Logs/temp_log.txt', 'a')
 	log.write('\n--------------------------------------------------------\n')
 	log.write('Running on {dt}\n'.format(dt=str(datetime.datetime.today())))
 
-	day_data_dicts = []
-	locations_df = pd.read_csv('Locations.csv')
-	selected_city = locations_df[(locations_df['City'] == city) & (locations_df['State'] == state) & (locations_df['Zip'] == zip)]
-	
-	if len(selected_city) == 0:
-		log.write("No city was found for {city},{state},{zip}\n\n".format(city=city,state=state,zip=zip))
-		log.write('--------------------------------------------------------\n')
-		log.close()
-		return
-
-	url = selected_city['Url'].item()
+	url = get_url(city, state, zip)
+	all_urls = []
 	
 	while start_date < end_date:
 		day = start_date.day
@@ -63,30 +82,15 @@ def get_city_daily_weather(city = 'Tulsa', state = 'Oklahoma', zip = 74104,
 		year = start_date.year
 
 		full_url = url.format(year=year, month=month, day=day)
-		log.write("\nLoading data for {dt} using {url}\n".format(url=full_url,dt=start_date))
-		print "Loading data for {dt} using {url}\n".format(url=full_url,dt=start_date)
-		
-		try:
-			day_dict = get_day_dict(full_url)
-			day_data_dicts.append(day_dict)
-			
-			log.write("Successfully loaded\n")
-		except:
-			log.write("ERROR:{0}\n".format(sys.exc_info()[0]))
+		all_urls.append(full_url)
 		
 		start_date = start_date + datetime.timedelta(1)
 	
-	days_df = pd.DataFrame()
-	
-	for day_dict in day_data_dicts:
-		days_df = days_df.append(pd.DataFrame(day_dict))
+	days_df = get_day_dicts_df(all_urls, log, city, state, zip)
 	
 	file_name = 'Data\historical_weather_{city}_{state}_{zip}.csv'.format(city=city,state=state,zip=zip).replace(' ','')
-	days_df['City'] = city
-	days_df['State'] = state
-	days_df['Zip'] = zip
-	
-	days_df.to_csv(file_name)
+
+	days_df.to_csv(file_name, index=False)
 	
 	endtime = datetime.datetime.today()
 	elapsed_time = endtime - starttime	
@@ -100,6 +104,75 @@ def get_city_daily_weather(city = 'Tulsa', state = 'Oklahoma', zip = 74104,
 	
 	return days_df
 
+def get_url(city, state, zip):
+	locations_df = pd.read_csv('Locations.csv')
+	selected_city = locations_df[(locations_df['City'] == city) & (locations_df['State'] == state) & (locations_df['Zip'] == zip)]
+	
+	if len(selected_city) == 0:
+		log.write("No city was found for {city},{state},{zip}\n\n".format(city=city,state=state,zip=zip))
+		log.write('--------------------------------------------------------\n')
+		log.close()
+		return
+
+	url = selected_city['Url'].item()
+	
+	return url
+	
+def find_and_add_missing_city_date_data_to_df(df, start_date, end_date, city, state, zip):
+	starttime = datetime.datetime.today()
+	log = open('Logs/historical_weather_loading_log.txt', 'a')
+	log.write('\n--------------------------------------------------------\n')
+	log.write('Loading missing dates for {city}, {state}, {zip}'.format(city=city,state=state,zip=zip))
+	url = get_url(city, state, zip)
+
+	try: url
+	except: 
+		log.write('City, state, zip not found')
+		log.close()
+		return
+
+	if len(df) == 0:
+		dates_for_location = []
+	else:
+		location_df = df[(df['City'] == city) & (df['Zip'] == zip) & (df['State'] == state)]
+		dates_for_location = pd.to_datetime(location_df['DateUTC']).dt.date.unique()
+
+	missing_dates = []
+
+	#Get any missing dates for the given city,state,zip within the given date range
+	while start_date <= end_date:
+		if start_date not in dates_for_location:
+			missing_dates.append(start_date)
+		
+		start_date = start_date + datetime.timedelta(1)
+
+	log.write("{num_missing} missing dates found\n".format(num_missing=len(missing_dates)))
+	print "{num_missing} missing dates found\n".format(num_missing=len(missing_dates))
+		
+	if len(missing_dates) == 0:
+		raise Exception("No missing dates found!  Ya done it.")
+	else:
+		all_missing_urls =[]
+		
+		#Create full urls based on missing dates
+		for missing_date in missing_dates:
+			full_url = url.format(year=missing_date.year, month=missing_date.month, day=missing_date.day)
+			all_missing_urls.append(full_url)
+			
+		new_data_df = get_day_dicts_df(all_missing_urls, log, city, state, zip)
+		
+		new_df = df.append(new_data_df)
+
+	endtime = datetime.datetime.today()
+	elapsed_time = endtime - starttime
+
+	log.write("\nTime to complete: {0}\n".format(str(elapsed_time)))
+	log.write('--------------------------------------------------------\n')    
+	log.close()
+
+	print "DONE"
+	return new_df
+	
 
 
 
@@ -111,8 +184,30 @@ def get_city_daily_weather(city = 'Tulsa', state = 'Oklahoma', zip = 74104,
 
 
 
-
-
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 
 
